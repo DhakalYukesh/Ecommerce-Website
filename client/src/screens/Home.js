98db9fff52f465ee1b7a8e61c3aab84eaa05b835
Yukesh Dhakal <yukeshdhakal42@gmail.com>
2023-09-27 11:41:36 +0545

    Added card, carousel, footer, navbar, home and login components.

---
+++ client/src/screens/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Carousel from "../components/Carousel";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+
+      <div>
+        <Carousel />
+      </div>
+
+      <div className="m-3">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
